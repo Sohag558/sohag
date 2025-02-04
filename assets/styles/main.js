292255358e0839c71d9e8f2c7de6fcaa2c0c092d
sohag Islam <154774449+Sohag558@users.noreply.github.com>
2025-02-05 02:36:53 +0600

    all done

---
+++ assets/styles/main.js
@@ -0,0 +1,12 @@
+
+  // window.addEventListener("scroll", function() {
+  //   const navbar = document.querySelector(".navbar");
+  //   if (window.scrollY > 50) {
+  //     navbar.style.background = "#2575fc"; 
+  //     navbar.style.boxShadow = "0 4px 6px rgba(0, 0, 0, 0.2)";
+  //   } else {
+  //     navbar.style.background = "transparent";
+  //     navbar.style.boxShadow = "none";
+  //   }
+  // });
+ 
