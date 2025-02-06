8a16636eb6effc9ad21a024f47b3ee68cb8eb901
sohag Islam <154774449+Sohag558@users.noreply.github.com>
2025-02-06 15:16:44 +0600

    add response to form

--- styles/main.js
+++ styles/main.js
@@ -1,12 +1,39 @@
 
-  // window.addEventListener("scroll", function() {
-  //   const navbar = document.querySelector(".navbar");
-  //   if (window.scrollY > 50) {
-  //     navbar.style.background = "#2575fc"; 
-  //     navbar.style.boxShadow = "0 4px 6px rgba(0, 0, 0, 0.2)";
-  //   } else {
-  //     navbar.style.background = "transparent";
-  //     navbar.style.boxShadow = "none";
-  //   }
-  // });
- 
+const form = document.getElementById('form');
+const result = document.getElementById('result');
+
+form.addEventListener('submit', function (e) {
+    e.preventDefault();
+    const formData = new FormData(form);
+    const object = Object.fromEntries(formData);
+    const json = JSON.stringify(object);
+    result.innerHTML = "Please wait..."
+
+    fetch('https://api.web3forms.com/submit', {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json',
+            'Accept': 'application/json'
+        },
+        body: json
+    })
+        .then(async (response) => {
+            let json = await response.json();
+            if (response.status == 200) {
+                result.innerHTML = "Form submitted successfully";
+            } else {
+                console.log(response);
+                result.innerHTML = json.message;
+            }
+        })
+        .catch(error => {
+            console.log(error);
+            result.innerHTML = "Something went wrong!";
+        })
+        .then(function () {
+            form.reset();
+            setTimeout(() => {
+                result.style.display = "none";
+            }, 3000);
+        });
+});
